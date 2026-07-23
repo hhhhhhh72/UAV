0c7675d49c460635411cf4952f8c6b8f7811cc2d
Developer <developer@drone-platform.local>
2026-07-23 22:15:53 +0800

    chore: 删除miniprogram/ — 重构前端,下次参考Test/microAPP-main模式

--- miniprogram/pages/messages/messages.js
+++
@@ -1,9 +0,0 @@
-const app = getApp()
-Page({
-  data: { messages: [] },
-  onShow() {
-    app.get('/api/v1/messages').then(res => {
-      this.setData({ messages: res.data || [] })
-    }).catch(() => {})
-  }
-})
--- miniprogram/pages/search/search.js
+++
@@ -1,1 +0,0 @@
-Page({ data: { keyword: '' }, onInput(e) { this.setData({ keyword: e.detail.value }) }, onSearch() { wx.showToast({ title: '搜索: ' + this.data.keyword, icon: 'none' }) } })
